c05550f620c7e28faf495cae960b6fc0fe7657a6
Piers Powlesland <piers.powlesland@yoti.com>
2018-05-13 17:47:16 +0100

    Add queries and update run_blockchain.sh script

--- run_blockchain.sh
+++ run_blockchain.sh
@@ -3,3 +3,5 @@ set -e
 composer archive create -t dir -n .
 composer network install --card PeerAdmin@hlfv1 --archiveFile grownyc@0.0.1.bna
 composer network start --networkName grownyc --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card 
+composer card import --file networkadmin.card
+composer network ping --card admin@grownyc  
